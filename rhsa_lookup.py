5c300be81e2d39dbb112b4fb29a0e1dd997c575e
SquanchIt <141355723+SquanchIt@users.noreply.github.com>
2023-09-28 13:58:13 -0400

    Update rhsa_lookup.py

--- rhsa_lookup.py
+++ rhsa_lookup.py
@@ -3,6 +3,7 @@ import sys
 import os
 import requests
 import configparser
+import json
 
 # Define the path to the configuration file
 config_file_path = '../etc/rhsa_lookup.conf'
@@ -21,68 +22,89 @@ if len(sys.argv) != 2:
 # Retrieve the RHSA ID from the command line argument and convert it to uppercase
 rhsa_id = sys.argv[1].upper()
 
-# Check if the configuration file exists
-if not os.path.isfile(config_file_path):
-    print(f"Error: Configuration file not found at {config_file_path}")
-    sys.exit(1)
+# Function to obtain the access token
+def get_access_token():
+    try:
+        # Read offline_token from a file
+        with open('offline_token.txt', 'r') as token_file:
+            offline_token = token_file.read().strip()
 
-# Read API token from the configuration file
-config = configparser.ConfigParser()
-config.read(config_file_path)
+        # Define the authentication request data
+        auth_data = {
+            "grant_type": "refresh_token",
+            "client_id": "rhsm-api",
+            "refresh_token": offline_token
+        }
 
-if 'Credentials' in config and 'api_token' in config['Credentials']:
-    api_token = config['Credentials']['api_token']
-else:
-    print("Error: API token not found in the configuration file")
-    sys.exit(1)
+        # Make a POST request to obtain the access token
+        response = requests.post(
+            "https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token",
+            data=auth_data
+        )
 
-# Construct the API URL to fetch advisory information
-api_url = f"https://api.access.redhat.com/management/v1/errata/{rhsa_id}"
+        # Check if the request was successful
+        if response.status_code == 200:
+            response_data = json.loads(response.text)
+            return response_data.get('access_token')
+        else:
+            print(f"Error obtaining access token. Status code: {response.status_code}")
+            return None
+    except Exception as e:
+        print(f"An error occurred while obtaining the access token: {str(e)}")
+        return None
 
-# Set up headers with authentication and "accept" header
-headers = {
-    "Authorization": f"Bearer {api_token}",
-    "accept": "application/json"
-}
+# Get the access token
+access_token = get_access_token()
 
-try:
-    # Make a GET request to the Red Hat Customer Portal API
-    response = requests.get(api_url, headers=headers)
+# Check if we successfully obtained the access token
+if access_token:
+    # Construct the API URL to fetch advisory information
+    api_url = f"https://api.access.redhat.com/management/v1/errata/{rhsa_id}"
 
-    # Check if the request was successful
-    if response.status_code == 200:
-        try:
-            # Attempt to parse the response as JSON
-            rhsa_data = response.json()
-            
-            # Extract and print RHSA information
-            print(f"RHSA ID: {rhsa_data.get('id')}")
-            print(f"Title: {rhsa_data.get('synopsis')}")
-            print(f"Description: {rhsa_data.get('description')}")
-            print(f"Issued: {rhsa_data.get('issued')}")
-            print(f"Severity: {rhsa_data.get('severity')}")
-            
-            # Extract and print affected products
-            print("\nAffected Products:")
-            for product in rhsa_data.get('affectedProducts', []):
-                print(f"Product: {product}")
-            
-            # Extract and print CVEs
-            print("\nCVEs:")
-            for cve in rhsa_data.get('cves', []):
-                print(f"CVE: {cve}")
-        except ValueError as ve:
-            print(f"Failed to parse JSON response: {ve}")
-    elif response.status_code == 400:
-        # Handle 400 Bad Request errors
-        error_description = response.json().get('error', {}).get('message')
-        print(f"Error 400: {error_description}")
-    elif response.status_code in [401, 402, 403, 404, 500]:
-        # Handle 401, 402, 403, 404, and 500 errors
-        error_description = response.json().get('error', {}).get('message')
-        print(f"Error {response.status_code}: {error_description}")
-    else:
-        print(f"Failed to retrieve RHSA information. Status code: {response.status_code}")
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+    # Set up headers with authentication and "accept" header
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "accept": "application/json"
+    }
+
+    try:
+        # Make a GET request to the Red Hat Customer Portal API
+        response = requests.get(api_url, headers=headers)
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            try:
+                # Attempt to parse the response as JSON
+                rhsa_data = response.json()
+
+                # Extract and print RHSA information
+                print(f"RHSA ID: {rhsa_data.get('id')}")
+                print(f"Title: {rhsa_data.get('synopsis')}")
+                print(f"Description: {rhsa_data.get('description')}")
+                print(f"Issued: {rhsa_data.get('issued')}")
+                print(f"Severity: {rhsa_data.get('severity')}")
+
+                # Extract and print affected products
+                print("\nAffected Products:")
+                for product in rhsa_data.get('affectedProducts', []):
+                    print(f"Product: {product}")
+
+                # Extract and print CVEs
+                print("\nCVEs:")
+                for cve in rhsa_data.get('cves', []):
+                    print(f"CVE: {cve}")
+            except ValueError as ve:
+                print(f"Failed to parse JSON response: {ve}")
+        elif response.status_code == 400:
+            # Handle 400 Bad Request errors
+            error_description = response.json().get('error', {}).get('message')
+            print(f"Error 400: {error_description}")
+        elif response.status_code in [401, 402, 403, 404, 500]:
+            # Handle 401, 402, 403, 404, and 500 errors
+            error_description = response.json().get('error', {}).get('message')
+            print(f"Error {response.status_code}: {error_description}")
+        else:
+            print(f"Failed to retrieve RHSA information. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
