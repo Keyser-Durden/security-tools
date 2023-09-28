4efbc8b1cf1757ac07ff9df4266a1cceea8baf19
SquanchIt <141355723+SquanchIt@users.noreply.github.com>
2023-09-28 16:58:15 -0400

    Update cve_lookup.sh

--- cve_lookup.sh
+++ cve_lookup.sh
@@ -12,22 +12,18 @@ cve_id=$(echo "$1" | tr '[:lower:]' '[:upper:]')
 # Construct the NVD API URL
 nvd_api_url="https://services.nvd.nist.gov/rest/json/cve/1.0/$cve_id"
 
-try {
-    # Send a GET request to the NVD API
-    response=$(curl -s "$nvd_api_url")
+# Send a GET request to the NVD API and capture the response
+response=$(curl -s "$nvd_api_url")
 
-    # Check if the request was successful
-    if [ "$(echo "$response" | jq -r '.statusCode')" == "200" ]; then
-        cve_description=$(echo "$response" | jq -r '.result.CVE_Items[0].cve.description.description_data[0].value')
-        cve_published_date=$(echo "$response" | jq -r '.result.CVE_Items[0].publishedDate')
+# Check if the request was successful
+if [ "$(echo "$response" | jq -r '.statusCode')" == "200" ]; then
+    cve_description=$(echo "$response" | jq -r '.result.CVE_Items[0].cve.description.description_data[0].value')
+    cve_published_date=$(echo "$response" | jq -r '.result.CVE_Items[0].publishedDate')
 
-        # Print the CVE information
-        echo "CVE ID: $cve_id"
-        echo "Description: $cve_description"
-        echo "Published Date: $cve_published_date"
-    else
-        echo "Failed to retrieve CVE information. Status code: $(echo "$response" | jq -r '.statusCode')"
-    fi
-} catch {
-    echo "An error occurred: $0"
-}
+    # Print the CVE information
+    echo "CVE ID: $cve_id"
+    echo "Description: $cve_description"
+    echo "Published Date: $cve_published_date"
+else
+    echo "Failed to retrieve CVE information. Status code: $(echo "$response" | jq -r '.statusCode')"
+fi
