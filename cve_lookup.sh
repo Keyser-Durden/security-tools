0c02b87d6a9fc577d96134bad791244478ceabc2
SquanchIt <141355723+SquanchIt@users.noreply.github.com>
2023-10-02 14:38:14 -0400

    Update cve_lookup.sh

--- cve_lookup.sh
+++ cve_lookup.sh
@@ -1,93 +1,83 @@
 #!/bin/bash
 
-# Initialize variables for flags
-cve_flag=false
-description_flag=false
-published_date_flag=false
-rhsa_flag=false
-verbose_flag=false
+# Initialize verbose flag to false
+verbose=false
 
-function show_usage {
-    echo "Usage: $0 [-c] [-d] [-p] [-r] [-v] <CVE-ID>"
-    echo "Options:"
-    echo "  -c    Show CVE ID"
-    echo "  -d    Show Description"
-    echo "  -p    Show Published Date"
-    echo "  -r    Show Related RHSA"
-    echo "  -v    Show output descriptions (e.g., 'Related RHSA:')"
-    exit 1
-}
-
-while getopts "cdprv" opt; do
-    case $opt in
-        c)
-            cve_flag=true
-            ;;
-        d)
-            description_flag=true
-            ;;
-        p)
-            published_date_flag=true
-            ;;
-        r)
-            rhsa_flag=true
-            ;;
-        v)
-            verbose_flag=true
-            ;;
-        \?)
-            show_usage
-            ;;
-    esac
+# Check for the -v flag
+while getopts ":v" opt; do
+  case $opt in
+    v)
+      verbose=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
 done
 
-# Shift to the next argument after processing flags
+# Shift the command-line arguments to exclude the processed options
 shift $((OPTIND-1))
 
-# Check if the CVE ID is provided
+# Check if the script is called without an argument
 if [ $# -ne 1 ]; then
-    show_usage
+  echo "Usage: $0 [-v] <RHSA>"
+  exit 1
 fi
 
-# Retrieve the CVE ID from the command line argument and convert it to uppercase
-cve_id=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+# Function to validate the RHSA format
+validate_rhsa_format() {
+    local rhsa_pattern="RHSA-[0-9]{4}:[0-9]+"
+    if [[ ! $1 =~ $rhsa_pattern ]]; then
+        echo "Invalid RHSA format. Please provide an RHSA in the format RHSA-<4 digits>:<digits>."
+        exit 1
+    fi
+}
 
-# Construct the NVD API URL
-nvd_api_url="https://services.nvd.nist.gov/rest/json/cve/1.0/$cve_id"
+# Validate the RHSA format
+validate_rhsa_format "$1"
 
-# Send a GET request to the NVD API and capture the response, then parse it with jq
-cve_info=$(curl -s "$nvd_api_url" | jq -r '.result.CVE_Items[0] | { "CVE ID": .cve.CVE_data_meta.ID, "Description": .cve.description.description_data[0].value, "Published Date": .publishedDate, "Related RHSA": .cve.references.reference_data[] | select(.refsource == "REDHAT") | .name }')
+# Read the offline token from the first line of ./etc/rh_api_token.txt
+offline_token=$(head -n 1 ./etc/rh_api_token.txt)
 
-# Check which flags were provided and display the corresponding information
-if [ -n "$cve_info" ]; then
-    if [ "$cve_flag" == true ]; then
-        if [ "$verbose_flag" == true ]; then
-            echo "CVE ID: $(echo "$cve_info" | jq -r '.["CVE ID"]')"
-        else
-            echo "$(echo "$cve_info" | jq -r '.["CVE ID"]')"
-        fi
-    fi
-    if [ "$description_flag" == true ]; then
-        if [ "$verbose_flag" == true ]; then
-            echo "Description: $(echo "$cve_info" | jq -r '.Description')"
-        else
-            echo "$(echo "$cve_info" | jq -r '.Description')"
-        fi
-    fi
-    if [ "$published_date_flag" == true ]; then
-        if [ "$verbose_flag" == true ]; then
-            echo "Published Date: $(echo "$cve_info" | jq -r '.["Published Date"]')"
-        else
-            echo "$(echo "$cve_info" | jq -r '.["Published Date"]')"
-        fi
-    fi
-    if [ "$rhsa_flag" == true ]; then
-        if [ "$verbose_flag" == true ]; then
-            echo "Related RHSA: $(echo "$cve_info" | jq -r '.["Related RHSA"]')"
-        else
-            echo "$(echo "$cve_info" | jq -r '.["Related RHSA"]')"
-        fi
-    fi
-else
-    echo "Failed to retrieve CVE information for CVE ID: $cve_id"
-fi
+# Convert the RHSA variable to uppercase (macOS compatible)
+RHSA=$(echo "$1" | tr '[:lower:]' '[:upper:]')
+
+# Define the function to obtain the access token
+function getAccessToken() {
+  curl_result=$(curl -s "https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token" \
+    -d grant_type=refresh_token -d client_id=rhsm-api -d refresh_token="$offline_token")
+  access_token=$(echo "$curl_result" | jq -r '.access_token')
+  echo "$access_token"
+}
+
+# Call the function to get the access token and store it in a variable
+token=$(getAccessToken)
+
+# Function to get RHSA information using the access token
+get_rhsa_info() {
+  # Use this URL to get data: https://api.access.redhat.com/management/v1/errata/$RHSA
+  # You can use the $token variable to include the access token in the request headers
+  json_response=$(curl -s -H "Authorization: Bearer $token" "https://api.access.redhat.com/management/v1/errata/$RHSA")
+
+  # Check if the "cves" field exists in the JSON response
+  if [[ $(echo "$json_response" | jq '.body.cves') != "null" ]]; then
+    # Extract the "cves" field and remove leading/trailing spaces
+    cves=$(echo "$json_response" | jq -r '.body.cves' | tr -d '[:space:]')
+
+    # Split the CVEs based on the "CVE-" prefix and print them one per line
+    cve_list=$(echo "$cves" | grep -o 'CVE-[0-9]\{4,9\}-[0-9]\{4,9\}' | tr '\n' ',' | sed 's/,$//')
+
+    # Output each CVE,RHSA pair on a separate line
+    for cve in $(echo "$cve_list" | tr ',' ' '); do
+      echo "$RHSA,$cve"
+    done
+  else
+    # Output "CVE,RHSA" if there are no related CVEs
+    echo "$RHSA,"
+  fi
+}
+
+# Call the get_rhsa_info function to retrieve RHSA information and print related CVEs
+get_rhsa_info
+
